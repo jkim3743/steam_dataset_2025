ad27ac99356a1f311f36f998ba7ca08a813b80dd
Jungkyu Jayden Kim <11654374+jkim3743@users.noreply.github.com>
2025-11-09 20:56:56 -0500

    Compute mat_discount_percent in dim_applications; ensure fact joins dim

--- models/marts/dim_applications.sql
+++ models/marts/dim_applications.sql
@@ -1,16 +1,34 @@
 {{ config(materialized='table') }}
 
-select
+with src as (
+  select
     appid,
     name,
     type,
-    is_free,
     release_date,
     mat_initial_price,
     mat_final_price,
-    mat_discount_percent,
-    mat_achievement_count,
-    mat_supports_windows,
-    mat_supports_mac,
-    mat_supports_linux
-from {{ ref('stg_applications') }}
+    is_free,
+    platform_windows,
+    platform_mac,
+    platform_linux
+  from {{ ref('stg_applications') }}
+)
+
+select
+  appid,
+  name,
+  type,
+  release_date,
+  mat_initial_price,
+  mat_final_price,
+  case
+    when mat_initial_price is null or mat_initial_price = 0 then 0
+    else round(100.0 * (mat_initial_price - mat_final_price) / mat_initial_price, 2)
+  end as mat_discount_percent,
+  is_free,
+  platform_windows,
+  platform_mac,
+  platform_linux
+from src;
+
--- models/marts/fact_reviews.sql
+++ models/marts/fact_reviews.sql
@@ -1,18 +1,13 @@
 {{ config(materialized='table') }}
 
 select
-    r.recommendationid,
-    r.appid,
-    r.voted_up,
-    r.votes_up,
-    r.votes_funny,
-    r.timestamp_created,
-    r.timestamp_updated,
-    a.name as app_name,
-    a.is_free,
-    a.release_date,
-    a.mat_final_price,
-    a.mat_discount_percent
-from {{ ref('stg_reviews') }} as r
-left join {{ ref('stg_applications') }} as a
-  on r.appid = a.appid
+  r.recommendationid,
+  r.appid,
+  coalesce(r.voted_up, r.votes_up, 0) as voted_up,
+  r.playtime_at_review,
+  r.timestamp_created,
+  coalesce(r.timestamp_updated, r.timestamp_created) as timestamp_updated,
+  a.mat_discount_percent
+from {{ ref('stg_reviews') }} r
+join {{ ref('dim_applications') }} a
+  using (appid);
