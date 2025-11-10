d0f27d36a14c1bcaa5972c82bd9e8e6b0ad371cd
Jungkyu Jayden Kim <11654374+jkim3743@users.noreply.github.com>
2025-11-09 21:19:38 -0500

    stg_applications: explicitly select and default columns incl. type

--- models/staging/stg_applications.sql
+++ models/staging/stg_applications.sql
@@ -1,5 +1,14 @@
 {{ config(materialized='view') }}
 
-select *
+select
+  cast(appid as integer)                              as appid,
+  name,
+  coalesce(type, 'game')                              as type,
+  try_cast(release_date as date)                      as release_date,
+  try_cast(mat_initial_price as double)               as mat_initial_price,
+  try_cast(mat_final_price  as double)                as mat_final_price,
+  coalesce(is_free, false)                            as is_free,
+  coalesce(platform_windows, false)                   as platform_windows,
+  coalesce(platform_mac,     false)                   as platform_mac,
+  coalesce(platform_linux,   false)                   as platform_linux
 from read_parquet('{{ env_var("DATA_ROOT") }}/dim_applications.parquet')
-
